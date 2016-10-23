ff5fce0a3f189f38740bcc66735447d7dcf49867
AlexN34 <alex-nguyen@live.com.au>
2016-10-23 23:39:12 +1100

    clean up code, add planning docs

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 # imports
-from flask import Flask, request, session, \
-    render_template, flash, jsonify
-# abort
+from flask import Flask, request, session, flash, jsonify
 from flask_api import status
 from flask_cors import CORS
 import time
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 
-# from flask_mysqldb import MySQL
-# import collections
 import MySQLdb
 import os
 # configuration of db
@@ -22,7 +18,6 @@ app.config['MYSQL_USER'] = 'comp4920'
 app.config['MYSQL_PASSWORD'] = 'q3H286cJ5EXyGqRw'
 app.config['MYSQL_DB'] = 'bookswapp'
 app.config.from_object(__name__)  # config from above variables in file
-# mysql = MySQL(app)  # attaches mysql object to the app?
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
@@ -35,46 +30,12 @@ CORS(app)
 
 def connection():
     # mysql object holds credentials, use it to connect to db
-    # conn = mysql.connection()
     conn = MySQLdb.connect(host=app.config['MYSQL_HOST'],
                            user=app.config['MYSQL_USER'],
                            passwd=app.config['MYSQL_PASSWORD'],
                            db=app.config['MYSQL_DB'])
-    # c = mysql.connection.cursor()
     c = conn.cursor()
     return c, conn
-
-
-@app.route("/")
-def index():
-    todo = """Current API:
-    /login - show login screen
-    /logout - works
-    /register - show registration form
-
-    /api/user/login -> logs user in; basic function works, needs hashing
-    /api/user/register -> Adds a new user to the database
-
-    /api/user/<userid> -> Retrieves user information
-    /api/user/list -> Lists all the users
-
-    /api/books/create -> Adds a new book to the database
-    /api/books/<bookid> -> Retrieves book information
-    /api/books/list -> Lists all the books
-    /api/books/search/<query> -> returns all results that any field matches
-
-    /api/listings -> Shows which user owns which book"""
-
-    if session.get('logged_in'):
-        userid = session['user_id']
-    else:
-        userid = 0
-
-        # Random login details while testing front-end
-        session['logged_in'] = True
-        session['user_id'] = 33
-        session['email'] = "bread@gmail.com"
-    return render_template('index.html', todo=todo, userid=userid)
 
 
 # User login/authentication/session management.
@@ -86,9 +47,6 @@ def login():
     # Check that email and password fields are not empty
     if request.method == 'POST' and request.form['email'] and \
        request.form['password']:
-        # Debugging: show input email
-        # flash(request.form['email'])
-        # flash(request.form['password'])
         query = ("SELECT * FROM User WHERE email = %s")
         email = "%s" % request.form['email']
 
@@ -102,12 +60,6 @@ def login():
                 error = "Invalid password"
             else:
                 token = generate_auth_token(rv[0])
-                # session['email'] = request.form['email']
-                # session['logged_in'] = True
-                # session['user_id'] = rv[0]
-                # flash("Success: you are now logged in")
-                # Back to list page on success with flash message
-                # return redirect(url_for('index'))
                 return jsonify({
                     'status': 200,
                     'message': 'Login successful',
@@ -119,27 +71,6 @@ def login():
             error = "Invalid email"
 
     return login_error(error)
-    # Point this to home page with errors if they occurred
-    # return render_template('index.html', error=error)
-
-
-@app.route('/logout')
-def logout():
-    """ User logout/authentication/session management. """
-    session.pop('email', None)
-    session.pop('logged_in', False)
-    session.pop('user_id', None)
-    flash('You were logged out')
-    # return redirect(url_for('index'))
-    return jsonify({
-        'status': 200,
-        'message': 'User logout successful',
-        })
-
-
-@app.route('/register')
-def show_registration():
-    return render_template('register.html')
 
 
 @app.route('/api/user/register', methods=['GET', 'POST'])
@@ -161,7 +92,6 @@ def register():
         error = "Email already exists"
         c.close()
         con.close()
-        # return redirect(url_for('index')), status.HTTP_400_BAD_REQUEST
         return registration_error(error)
 
     # Create the user return success status
@@ -177,11 +107,6 @@ def register():
         c.close()
         con.close()
 
-        flash("Just registered new user. Details are:")
-        flash(request.form['email'])
-        flash(request.form['password'])
-        flash(request.form['university'])
-        flash(request.form['location'])
         # return redirect(url_for('index')), status.HTTP_201_CREATED
         return jsonify({
             'status': 201,
@@ -216,15 +141,6 @@ def get_user(userid):
 @app.route('/api/user/update/<userid>', methods=['POST'])
 def update_user(userid):
     message = ''
-
-    # # Check user is logged in
-    # if not session.get('logged_in'):
-    #     return not_logged_in()
-
-    # # Check logged in user is the one being updated
-    # if str(userid) != str(session['user_id']):
-    #     return not_auth()
-
     # Check user is logged in (has a valid token)
     if not verify_auth_token(request.form['token']):
         return not_logged_in()
@@ -284,13 +200,6 @@ def update_user(userid):
 # Using GET method for now for easier testing
 @app.route('/api/user/delete/<userid>', methods=['POST'])
 def delete_user(userid):
-    # Check user is logged in
-    # if not session.get('logged_in'):
-    #     return not_logged_in()
-
-    # # Check logged in user is the one being deleted
-    # if str(userid) != str(session['user_id']):
-    #     return not_auth()
 
     # Check user is logged in (has a valid token)
     if not verify_auth_token(request.form['token']):
@@ -322,7 +231,6 @@ def delete_user(userid):
     con.commit()
     c.close()
     con.close()
-    logout()
     return jsonify({
         'status': 200,
         'message': 'User deleted',
@@ -338,7 +246,6 @@ def get_userlist():
     con.close()
     finalState = status.HTTP_204_NO_CONTENT
 
-    # http://codehandbook.org/working-with-json-in-python-flask/
     userList = []
     for user in rv:
         userDict = {
@@ -357,7 +264,6 @@ def get_userlist():
     return jsonify(userList), finalState
 
 
-# http://flask.pocoo.org/docs/0.11/patterns/fileuploads/
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -427,10 +333,6 @@ def add_wishlist():
 
 @app.route('/api/books/create', methods=['POST'])
 def add_book():
-    # if not session.get('logged_in'):
-    #     return not_logged_in()
-
-    # Check user is logged in (has a valid token)
     if not verify_auth_token(request.form['token']):
         return not_logged_in()
 
@@ -445,16 +347,11 @@ def add_book():
 
     # Optional parameters
     isbn = request.form.get('isbn', None)
-    # bookStatus = request.form.get('status', None)
     edition = request.form.get('edition', None)
     description = request.form.get('description', None)
-    # margin = request.form.get('margin', None)
 
     c, con = connection()
-    # http://dev.mysql.com/doc/refman/5.7/en/keywords.html
     # 'condition' is a reserved keyboard, have to put it in backticks
-    # http://stackoverflow.com/questions/21046293/
-    # error-in-sql-syntax-for-python-and-mysql-on-insert-operation
     query = ("INSERT INTO Book "
              "(name, author, isbn, prescribed_course, edition, `condition`, "
              "transaction_type, price, description) "
@@ -543,8 +440,6 @@ def update_book(bookid):
     return not_found()
 
 
-# @app.route('/api/books/delete/<bookid>', methods=['DELETE'])
-# Using GET method for now for easier testing
 @app.route('/api/books/delete/<bookid>', methods=['POST'])
 def delete_book(bookid):
     # Check user is logged in (has a valid token)
@@ -618,7 +513,6 @@ def get_book(bookid):
 def book_search(query):
     c, con = connection()
     search_string = '\'%' + query + '%\''
-    # values = (search_string, search_string, search_string, search_string)
     q = ("SELECT * FROM Book WHERE (name LIKE {0} OR author LIKE {1} "
          "OR isbn LIKE {2} "
          "OR prescribed_course LIKE {3})").format(search_string,
@@ -630,7 +524,6 @@ def book_search(query):
     c.close()
     con.close()
     finalState = status.HTTP_204_NO_CONTENT
-    # http://codehandbook.org/working-with-json-in-python-flask/
     bookList = []
     for book in rv:
         bookDict = {
@@ -692,29 +585,6 @@ def get_booklist(transaction_type):
     c.close()
     con.close()
     return jsonify(bookList)
-
-
-# Testing purposes
-@app.route('/api/listings')
-def get_listings():
-    c, con = connection()
-    query = ("SELECT * FROM Book_List")
-    c.execute(query)
-    rv = c.fetchall()
-
-    listings = []
-    for item in rv:
-        listingDict = {
-            'listing_id': item[0],
-            'user_id': item[1],
-            'book_id': item[2],
-            'date': item[3],
-        }
-        listings.append(listingDict)
-
-    c.close()
-    con.close()
-    return jsonify(listings)
 
 
 # Testing JOIN
@@ -799,43 +669,9 @@ def get_book_listing(bookid):
     con.close()
     return not_found()
 
-# @app.route('/api/request/<book_id>')
-# def request_book(book_id):
-#     # TODO how to retrieve requesting user? Assume retrieving from a form
-#     buying_user_id = request.form['buying_user_id']
-#     price = request.form['price']
-#     request_status = "Pending"
-#     c, con = connection()
-#     query = ("SELECT * FROM Book_List WHERE book_id = %s" % (book_id))
-#     c.execute(query)
-#     rv = c.fetchall()
-#     if rv:
-#         selling_user_id = rv[0][1]  # TODO make cleaner
-#         query = ("INSERT INTO Transaction_List "
-#                  "(buying_user_id, selling_user_id, price, status)"
-#                  "VALUES (%s, %s, %s, %s)")
-#         values = (buying_user_id, selling_user_id, price, request_status)
-#         c.execute(query, values)
-
-#      """ Does Transaction List need to know book ID/transaction? how to match
-#             Notification with Transaction List to retrieve price? """
-#         query = ("INSERT INTO Notification "
-#                  "(user_id, book_id)"
-#                  "VALUES (%s, %s)")
-#         values = (selling_user_id, book_id)
-#         c.execute(query, values)
-#         con.commit()
-#         c.close()
-#         con.close()
-
-#     else:
-#         return not_found()
-
 
 @app.route('/api/request/<book_id>', methods=['POST'])
 def request_book(book_id):
-    # TODO how to retrieve requesting user? Assume retrieving from a form
-    # Check token
     if not verify_auth_token(request.form['token']):
         return not_logged_in()
 
@@ -879,7 +715,6 @@ def response_book(notification_id):
     # Skip request is the same as the logged in user from the token (?)
     # Action contains accept/reject option
     action = request.form['action']
-    # action = 'accept'
     c, con = connection()
     # Check notification type first: If request, then send response
     # If Match, then return contact details inside json object?
@@ -898,7 +733,6 @@ def response_book(notification_id):
                      "WHERE Notification.Id= %s")
             c.execute(query, [notification_id])
             match = c.fetchone()
-            # Remove Book/notification??
             return jsonify({
                 'status': 200,
                 'message': match[0],
@@ -1074,7 +908,6 @@ def get_transactions(user_id):
     return jsonify(transactions)
 
 
-# https://blog.miguelgrinberg.com/post/restful-authentication-with-flask
 # Generates a token with the user_id as data and an expiration time of 10 minute
 def generate_auth_token(user_id, expiration=600):
     s = Serializer(app.secret_key, expires_in=expiration)
@@ -1149,8 +982,5 @@ def not_found():
     resp.status_code = 404
     return resp
 
-# @app.route('/test')
-# def testJson():
-    # return render_template('test.html')
 if __name__ == "__main__":
     app.run(debug=True)
